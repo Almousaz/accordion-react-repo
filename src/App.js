eb720908a9b92ec4333762ed7bd004afa22cdcdf
Ali Mousazada <mousazada.ali@gmail.com>
2024-05-16 21:13:39 -0400

    create load more data with use state and use effect

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Accordian from './Components/Accordion';
 import RandomColor from './Components/Random-color';
 import StarRating from './Components/Star Rating';
 import ImageSlider from './Components/image-slider';
+import LoadMoreData from './Components/load-more-data';
 
 
 
@@ -20,7 +21,10 @@ function App() {
       {/* <StarRating /> */}
 
       {/* image slider Components */}
-      <ImageSlider url={'http://picsum.photos/v2/list'} page={'1'} limit={'10'} />
+      {/* <ImageSlider url={'http://picsum.photos/v2/list'} page={'1'} limit={'10'} /> */}
+
+      {/* load more products Components */}
+      <LoadMoreData />
 
 
      
